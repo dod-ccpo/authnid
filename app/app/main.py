852e29db0801b1790fddb987b84ded50083fae36
Jason Garber <jason@promptworks.com>
2018-05-25 10:14:10 -0400

    Hello world root route

--- app/app/main.py
+++ app/app/main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 # Setup app
 from .core import app_setup  # noqa
 
+@app.route('/')
+def hello():
+    return "Hello World!"
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
