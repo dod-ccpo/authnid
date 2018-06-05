49171d65669e2fcb40c00c4ea7d02ba803700493
dandds <dan-ctr@friends.dds.mil>
2018-06-05 16:58:35 -0400

    implements client cert validation

--- authnid/make_app.py
+++ authnid/make_app.py
@@ -6,7 +6,9 @@ config_defaults = {
     'PORT': 4567,
     'DEBUG': True,
     'TESTING': False,
-    'ATST_REDIRECT': 'https://www.atat.codes/home'
+    'ATST_REDIRECT': 'https://www.atat.codes/home',
+    'CRL_DIRECTORY': 'crl',
+    'CA_CHAIN': '/app/ssl/server-certs/ca-chain.pem'
 }
 
 def make_app(config):
