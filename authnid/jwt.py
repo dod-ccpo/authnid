df7dbe63d6f5d4e5f5765952afd7b503cf9fabd4
dandds <dan-ctr@friends.dds.mil>
2018-06-07 15:35:46 -0400

    very basic token validation method

--- authnid/jwt.py
+++ authnid/jwt.py
@@ -6,3 +6,10 @@ class JWTManager():
 
     def token(self):
         return jwt.encode({'some': 'payload'}, self._secret).decode('utf-8')
+
+    def validate(self, token):
+        try:
+            jwt.decode(token, self._secret)
+            return True
+        except jwt.exceptions.InvalidTokenError:
+            return False
--- test/test_jwt.py
+++ test/test_jwt.py
@@ -1,7 +1,16 @@
 from authnid.jwt import JWTManager
 from test.helpers import is_jwt
 
+jwt_maker = JWTManager('some-secret')
+
 def test_can_build_jwt():
-    jwt_maker = JWTManager('some-secret')
     token = jwt_maker.token()
     assert is_jwt(token)
+
+def test_can_validate_jwt():
+    token = jwt_maker.token()
+    assert jwt_maker.validate(token)
+    assert not jwt_maker.validate('abc.123.def')
+    parts = token.split('.')
+    bad_token = '.'.join([parts[0], parts[1], 'somethingelseentirely'])
+    assert not jwt_maker.validate(bad_token)
